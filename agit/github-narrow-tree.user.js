634bb07d2bcafca2f004b6665c4bbf5cea948f07
Xbigo <wing123fire@gmail.com>
2025-09-05 13:13:27 -0400

    add github tree resize

---
+++ agit/github-narrow-tree.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         GithubNarrowTree
+// @namespace    http://tampermonkey.net/
+// @version      2025-07-29
+// @description  Narrow tree view for Github review.
+// @author       You
+// @match        https://git.autodesk.com/*/*/pull/*/files
+// @match        https://git.autodesk.com/*/*/pull/*/commits/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=autodesk.com
+// @run-at       context-menu
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var element = document.querySelector('div[data-target="diff-layout.sidebarContainer"]')
+    var newWidth = element.offsetWidth - 100;
+    element.style.width = newWidth +'px'
+    console.log(newWidth +'px')
+})();
